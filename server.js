a17308b92a8e0c5c7c7686a6e330c11609f96b4d
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-07-26 20:07:14 -0400

    added comments

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
+//the following app.get gets the to-do list and filters out any items where completed=true
 app.get('/',async (request, response)=>{
     const todoItems = await db.collection('todos').find().toArray()
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
@@ -35,6 +35,7 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+//the following app.post updates the database with a new list item from the user
 app.post('/addTodo', (request, response) => {
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
@@ -44,6 +45,7 @@ app.post('/addTodo', (request, response) => {
     .catch(error => console.error(error))
 })
 
+//this app.put is called when an item is marked complete, and alerts the console.log and the UI
 app.put('/markComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
@@ -61,6 +63,7 @@ app.put('/markComplete', (request, response) => {
 
 })
 
+//opposite of above, this time marks an item from complete to incomplete
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
         $set: {
@@ -71,13 +74,14 @@ app.put('/markUnComplete', (request, response) => {
         upsert: false
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Incomplete')
+        response.json('Marked Incomplete')
     })
     .catch(error => console.error(error))
 
 })
 
+//delete an item from the list
 app.delete('/deleteItem', (request, response) => {
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
@@ -88,6 +92,7 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
+//listen to heroku or local port
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
